1c34c991664d82aca794cf48b9146169e388c1c6
zilen-modi <zilenmodi@gmail.com>
2024-08-09 19:17:45 +0530

    chore: setup next.js app with typescript, add eslint, prettier, and lint-staged

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <p className="text-3xl font-bold">NEXT JS APP BOILER PLATE</p>
+    </main>
+  )
+}
